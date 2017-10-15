8c788fdeade9333d96df2d6f8c0ac5626db469d5
Ben Olden-Cooligan <ben.cyanfish@gmail.com>
2017-10-14 20:03:37 -0400

    Use config file for params and timestamp reports

--- analyze.py
+++ analyze.py
@@ -2,16 +2,7 @@ from models import *
 import chess, chess.pgn
 import json
 from collections import defaultdict
-
-book_depth = 10
-forced_move_thresh = 50
-unclear_pos_thresh = 100
-undecided_pos_thresh = 200
-losing_pos_thresh = 500
-exclude_forced = True
-include_only_unclear = True
-exclude_flat = True
-max_cpl = 500
+from datetime import datetime
 
 class PgnSpyResult():
 
@@ -46,15 +37,23 @@ class PgnSpyResult():
         return self.sample_total_cpl / float(self.sample_size) if self.sample_size else None
 
 def a1(working_set):
+    p = load_a1_params()
     by_player = defaultdict(PgnSpyResult)
     by_game = defaultdict(PgnSpyResult)
+    excluded = included = 0
     for gid, game in working_set.items():
         game_obj, _ = Game.get_or_create(id=gid)
+        if not game_obj.is_analyzed:
+            excluded += 1
+            continue
 
-        a1_game(by_player, by_game, game_obj, 'w', GamePlayer.get(game=game_obj, color='w').player)
-        a1_game(by_player, by_game, game_obj, 'b', GamePlayer.get(game=game_obj, color='b').player)
-    
-    with open('reports/testrep.txt', 'w') as fout:
+        a1_game(p, by_player, by_game, game_obj, 'w', GamePlayer.get(game=game_obj, color='w').player)
+        a1_game(p, by_player, by_game, game_obj, 'b', GamePlayer.get(game=game_obj, color='b').player)
+        included += 1
+    print(f'Skipping {excluded} games that haven\'t been pre-processed')
+
+    out_path = f'reports/report-a1--{datetime.now():%Y-%m-%d--%H-%M-%S}.txt'
+    with open(out_path, 'w') as fout:
         fout.write('------ BY PLAYER ------\n\n')
         for player, result in sorted(by_player.items(), key=lambda i:-i[1].t3_count/(i[1].t3_total or 1)):
             fout.write(f'{player.username}\n')
@@ -82,8 +81,9 @@ def a1(working_set):
                 fout.write(f'ACPL: {result.acpl:.1f} ({result.sample_size})\n')
             fout.write(' '.join(result.game_list) + '\n')
             fout.write('\n')
+    print(f'Wrote report on {included} games to "{out_path}"')
 
-def a1_game(by_player, by_game, game_obj, color, player):
+def a1_game(p, by_player, by_game, game_obj, color, player):
     moves = list(Move.select().where(Move.game == game_obj).order_by(Move.number, -Move.color))
 
     r = PgnSpyResult()
@@ -96,26 +96,26 @@ def a1_game(by_player, by_game, game_obj, color, player):
             continue
         evals.append(m.pv1_eval)
 
-        if m.number <= book_depth:
+        if m.number <= p['book_depth']:
             continue
 
-        if m.pv1_eval <= -undecided_pos_thresh or m.pv1_eval >= undecided_pos_thresh:
+        if m.pv1_eval <= -p['undecided_pos_thresh'] or m.pv1_eval >= p['undecided_pos_thresh']:
             continue
-        if m.pv2_eval and m.pv1_eval <= m.pv2_eval + forced_move_thresh and m.pv1_eval <= m.pv2_eval + unclear_pos_thresh:
+        if m.pv2_eval and m.pv1_eval <= m.pv2_eval + p['forced_move_thresh'] and m.pv1_eval <= m.pv2_eval + p['unclear_pos_thresh']:
             r.t1_total += 1
             if m.played_rank and m.played_rank <= 1:
                 r.t1_count += 1
-        if m.pv3_eval and m.pv2_eval <= m.pv3_eval + forced_move_thresh and m.pv1_eval <= m.pv3_eval + unclear_pos_thresh:
+        if m.pv3_eval and m.pv2_eval <= m.pv3_eval + p['forced_move_thresh'] and m.pv1_eval <= m.pv3_eval + p['unclear_pos_thresh']:
             r.t2_total += 1
             if m.played_rank and m.played_rank <= 2:
                 r.t2_count += 1
-        if m.pv4_eval and m.pv3_eval <= m.pv4_eval + forced_move_thresh and m.pv1_eval <= m.pv4_eval + unclear_pos_thresh:
+        if m.pv4_eval and m.pv3_eval <= m.pv4_eval + p['forced_move_thresh'] and m.pv1_eval <= m.pv4_eval + p['unclear_pos_thresh']:
             r.t3_total += 1
             if m.played_rank and m.played_rank <= 3:
                 r.t3_count += 1
 
-        cpl = min(max(m.pv1_eval - m.played_eval, 0), max_cpl)
-        if exclude_flat and cpl == 0 and evals[-3:] == [m.pv1_eval] * 3:
+        cpl = min(max(m.pv1_eval - m.played_eval, 0), p['max_cpl'])
+        if p['exclude_flat'] and cpl == 0 and evals[-3:] == [m.pv1_eval] * 3:
             # Exclude flat evals from CPL, e.g. dead drawn endings
             continue
 
@@ -128,3 +128,7 @@ def a1_game(by_player, by_game, game_obj, color, player):
     
     by_player[player].add(r)
     by_game[(player, game_obj.id)].add(r)
+
+def load_a1_params():
+    with open('./config/params_for_a1.json') as config_f:
+        return json.load(config_f)
